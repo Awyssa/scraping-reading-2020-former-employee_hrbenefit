ac8c9aca57bbe3f707bccda1539c11a08974fbfe
Michael Ernest <michael.ehson@gmail.com>
2021-03-01 19:28:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,5 +13,7 @@ courses.each do |course|
   puts course.text.strip
 end
 
+puts courses
 
-puts p doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")[0].attributes
+
+# puts p doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")[0].attributes
